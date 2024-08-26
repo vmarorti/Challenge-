66c42c21b79a581fa4466cc368acb62e89dca18b
vmarorti <marsmotionless98@gmail.com>
2024-08-26 04:17:57 -0500

    Added code to index.js and generateMarkdown.js

--- index.js
+++ index.js
@@ -1,13 +1,123 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+        message: "Enter your Title",
+        name: "title",
+        validate: title => {
+            if (title) {
+                return true;
+            } else {
+                console.log('You must enter a title.');
+                return false;
+            }
+        }
+    },
+    {
+        message: "Enter your Description",
+        name: "description",
+        validate: description => {
+            if (description) {
+                return true;
+            } else {
+                console.log('You must enter a description.');
+                return false;
+            }
+        }
+    },
+    {
+        message: "Enter your Installation instructions",
+        name: "install",
+        validate: install => {
+            if (install) {
+                return true;
+            } else {
+                console.log('You must enter an install.');
+                return false;
+            }
+        }
+    },
+    {
+        message: "Enter your Usage content",
+        name: "usage",
+        validate: usage => {
+            if (usage) {
+                return true;
+            } else {
+                console.log('You must enter a usage.');
+                return false;
+            }
+        }
+    },
+    {
+        type: "list",
+        message: "Choose your License",
+        name: "license",
+        choices: ["Apache-2.0", "EPL-1.0", "ISC", "MIT", "OFL-1.1", "Zlib"],
+        validate: license => {
+            if (license) {
+                return true;
+            } else {
+                console.log('You must enter a license.');
+                return false;
+            }
+        }
+    },
+    {
+        message: "Enter your Contribution credits",
+        name: "contributing",
+        validate: contributing => {
+            if (contributing) {
+                return true;
+            } else {
+                console.log('You must enter a contributing.');
+                return false;
+            }
+        }
+    },
+    {
+        message: "Enter your test descriptions",
+        name: "test",
+        validate: test => {
+            if (test) {
+                return true;
+            } else {
+                console.log('You must enter a test.');
+                return false;
+            }
+        }
+    },
+    {
+        message: "Provide your GitHub username",
+        name: "questions",
+        validate: questions => {
+            if (questions) {
+                return true;
+            } else {
+                console.log('You must enter a username.');
+                return false;
+            }
+        }
+    }
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data) , (err) => {
+        err ? console.log(err) : console.log('ReadMe has been created!')
+    });
 
+};
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer
+        .prompt(questions)
+            .then((data) => {
+                const fileName = "README(test).md";
+                writeToFile(fileName, data);
+            })
+};
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `(https://opensource.org/licenses/${license})`;
+  }
+};
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  const licenseSection = `${renderLicenseBadge(license)}\n${renderLicenseLink(license)}`
+  if (!license) {
+    return '';
+  } else {
+    return licenseSection;
+  }
+};
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
-}
+
+  ## Description
+
+  ${data.description}
+  
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  
+  ## Installation
+  
+  ${data.install}
+  
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  
+  ## License
+  
+  ${renderLicenseSection(data.license)}
+  
+  
+  ## Contributions
+  
+  ${data.contributing}
+  
+  
+  ## Tests
+  
+  ${data.test}
+  
+  
+  ## Questions
+  
+  [${data.questions}](https://github.com/${data.questions})
+  `
+  };
 
 module.exports = generateMarkdown;
